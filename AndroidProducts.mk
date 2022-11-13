c50e07596877884fcdd3c80671b412886e071a50
danzox <93202212+danzox@users.noreply.github.com>
2022-11-14 06:22:56 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Mi8937.mk
+    $(LOCAL_DIR)/aosp_Mi8937.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Mi8937-user \
-    lineage_Mi8937-userdebug \
-    lineage_Mi8937-eng
+    aosp_Mi8937-user \
+    aosp_Mi8937-userdebug \
+    aosp_Mi8937-eng
